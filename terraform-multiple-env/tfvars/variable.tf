c366442b2d98c761201b8fec091172dc9a292388
sruthakeerthi <sruthakeerthi53@gmail.com>
2025-02-20 15:19:27 +0800

    terraform

---
+++ terraform-multiple-env/tfvars/variable.tf
@@ -0,0 +1,25 @@
+variable "project"{
+    default = "expense"
+}
+variable "environment" {
+    
+}
+variable "instances" {
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+    default = "Z016876422U7W1OGZ1MFM"
+}
+
+variable "domain_name" {
+    default = "tskdaws.online"
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Terraform = "true"
+    }
+}
